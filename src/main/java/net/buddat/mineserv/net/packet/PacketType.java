2918e7c83df0d2b77d0a3877555ab1363c8623fa
Taufiq Hoven <taufiq.hoven@gmail.com>
2011-08-29 08:29:10 +1000

    Added remainder of javadoc to PacketType enums.

--- src/main/java/net/buddat/mineserv/net/packet/PacketType.java
+++ src/main/java/net/buddat/mineserv/net/packet/PacketType.java
@@ -94,48 +94,242 @@ public enum PacketType {
 	 */
 	PLAYER_POSITION_LOOK(0xD, 42),
 
+	/**
+	 * Client-initiated when the player mines a block.
+	 */
 	PLAYER_DIGGING(0xE, 12),
+
+	/**
+	 * Client-initiated when the player places a block or item, or when using
+	 * a block/item.
+	 */
 	PLAYER_BLOCK_PLACEMENT(0xF),
+
+	/**
+	 * Client-initiated when the player changes the active slot selection.
+	 */
 	HOLDING_CHANGE(0x10, 3),
+
+	/**
+	 * Server-initiated when two or more players are on a server and at least
+	 * one of them uses a bed.
+	 */
 	USE_BED(0x11, 15),
+
+	/**
+	 * Client-initiated when an entity is to change its animation.
+	 */
 	ANIMATION(0x12, 6),
+
+	/**
+	 * Client-initiated when an entity performs an action.
+	 */
 	ENTITY_ACTION(0x13, 6),
+
+	/**
+	 * Server-initiated when a named entity (ie. player) comes into visible
+	 * range. This must not ever refer to the receiver's own username or entity
+	 * id.
+	 */
 	NAMED_ENTITY_SPAWN(0x14),
+
+	/**
+	 * Server-initiated when an item comes into visible item range of the
+	 * player.
+	 */
 	PICKUP_SPAWN(0x15, 25),
+
+	/**
+	 * Server-initiated when an item is picked up by a player, including
+	 * both the receiver and players other than the receiver.
+	 */
 	COLLECT_ITEM(0x16, 9),
+
+	/**
+	 * Server-initiated when an object/vehicle is created.
+	 */
 	ADD_OBJECT_VEHICLE(0x17),
+
+	/**
+	 * Server-initiated when a mob entity is spawned.
+	 */
 	MOB_SPAWN(0x18),
+
+	/**
+	 * Shows the location, name and type of painting.
+	 */
 	ENTITY_PAINTING(0x19),
+
+	/**
+	 * Does not appear to be used.
+	 */
 	STANCE_UPDATE(0x1B),
+
+	/**
+	 * Entity velicity/motion.
+	 */
 	ENTITY_VELOCITY(0x1C, 11),
+
+	/**
+	 * Server-initiated when an entity is to be destroyed.
+	 */
 	DESTROY_ENTITY(0x1D, 5),
+
+	/**
+	 * Server-initiated to indicate that the entity did not move/look since
+	 * this or any other entity packet.
+	 */
 	ENTITY(0x1E, 5),
+
+	/**
+	 * Server-initiated when an entity moves at most four blocks. For
+	 * movement of more than four blocks {@link #ENTITY_TELEPORT} is used.
+	 */
 	ENTITY_RELATIVE_MOVE(0x1F, 8),
+
+	/**
+	 * Server-initiated when an entity rotates.
+	 */
 	ENTITY_LOOK(0x20, 7),
+
+	/**
+	 * Server-initiated when an entity both rotates and moves up to four
+	 * blocks.
+	 */
 	ENTITY_LOOK_RELATIVE_MOVE(0x21, 10),
+
+	/**
+	 * Server-initiated when an entity moves more than four blocks.
+	 */
 	ENTITY_TELEPORT(0x22, 19),
+
+	/**
+	 * Server-initiated indicating entity damage, death and explosion.
+	 */
 	ENTITY_STATUS(0x26, 6),
+
+	/**
+	 * Server-initiated indicated that a player is attached to an entity.
+	 */
 	ATTACH_ENTITY(0x27, 9),
+
+	/**
+	 * Extra entity information.
+	 */
 	ENTITY_METADATA(0x28),
+
+	/**
+	 * Server-initiated notifying the client to initialise or unload a chunk.
+	 */
 	PRE_CHUNK(0x32, 10),
+
+	/**
+	 * Server-initiated for the client to overwrite all or part of a chunk with
+	 * the contents of the packet.
+	 */
 	MAP_CHUNK(0x33),
+
+	/**
+	 * Multiple blocks changed.
+	 */
 	MULTI_BLOCK_CHANGE(0x34),
+
+	/**
+	 * Block changed.
+	 */
 	BLOCK_CHANGE(0x35, 12),
+
+	/**
+	 * Server-initiated when a block performs an action (eg. note block
+	 * played or a piston changed state).
+	 */
 	BLOCK_ACTION(0x36, 13),
+
+	/**
+	 * Server-initiated when an explosion occurs.
+	 */
 	EXPLOSION(0x3C),
+
+	/**
+	 * Server-initiated when a client is to play an effect.
+	 */
 	SOUND_EFFECT(0x3D, 18),
+
+	/**
+	 * Server-initiated when a bed can't be used as a spawn point and when the
+	 * rain state changes.
+	 */
 	NEW_INVALID_STATE(0x46, 2),
+
+	/**
+	 * Server-initiated when a thunderbolt strikes nearby.
+	 */
 	THUNDERBOLT(0x47, 18),
+
+	/**
+	 * Server-initiated when the client should open an inventory window.
+	 */
 	OPEN_WINDOW(0x64),
+
+	/**
+	 * Client-initiated when the an inventory window is closed, and
+	 * server-initiated when an inventory window should be forcible
+	 * closed.
+	 */
 	CLOSE_WINDOW(0x65, 2),
+
+	/**
+	 * Client-initiated when the player clicks on a slot in an inventory
+	 * window.
+	 */
 	WINDOW_CLICK(0x66),
+
+	/**
+	 * Server-initiated when an item is added to or removed from an inventory
+	 * window slot. Only partially documented.
+	 */
 	SET_SLOT(0x67),
+
+	/**
+	 * Server-initiated when an item is added to or removed from an inventory
+	 * window slot.
+	 */
 	WINDOW_ITEMS(0x68),
+
+	/**
+	 * Server-initiated when a furnace progress bar is updated.
+	 */
 	UPDATE_PROGRESS_BAR(0x69, 6),
+
+	/**
+	 * Server-initiated indicating whether a request from the client was
+	 * accepted or rejected (due to conflict). Client-initiated in response
+	 * to a server packet of the same type.
+	 */
 	TRANSACTION(0x6A, 5),
+
+	/**
+	 * Server-initiated when a sign is discovered or created. Client-initiated
+	 * when a signs has been labelled (by pressing the "Done" button after
+	 * placing a sign).
+	 */
 	UPDATE_SIGN(0x82),
+
+	/**
+	 * Server-initiated to specify complex data on an item, currently only
+	 * applicable to maps.
+	 */
 	ITEM_DATA(0x83),
+
+	/**
+	 * Server-initiated to increment a statistic.
+	 */
 	INCREMENT_STATISTIC(0xC8, 6),
+
+	/**
+	 * Server-initiated before it disconnects a client, and client-initiated
+	 * before it disconnects.
+	 */
 	DISCONNECT_KICK(0xFF),
 	;
 
